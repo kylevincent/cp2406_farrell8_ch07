3a65a0d4374705dc3f3930980c31dbd49cfd5d83
kyle <kyle.vincent@my.jcu.edu.au>
2017-09-01 09:08:00 +1000

    Finished task 3

--- Exercises/CountMovieSpaces.java
+++ Exercises/CountMovieSpaces.java
@@ -1,19 +1,30 @@
+import javax.swing.*;
+
 /**
  * Created by jc431075 on 31/08/17.
  */
 public class CountMovieSpaces {
     public static void main(String[] args)
     {
-        String myFavQuote = "Oh hi Mark!";
+        String myFavQuote = getUserInput();
         int numSpaces = 0;
+        char secretChar = ' ';
         int quoteLength = myFavQuote.length();
         for (int i = 0; i < quoteLength; i++){
-            if (myFavQuote == " "){
+            if (myFavQuote.charAt(i) == secretChar)
+            {
                 numSpaces++;
                 i++;
             }
-        }
 
+        }
+        System.out.println("NUMBER OF SPACES IN '" + myFavQuote + "' IS: " + numSpaces);
+
+    }
+    public static String getUserInput()
+    {
+        String usersQuote = JOptionPane.showInputDialog(null, "whats ya fav quote mate?");
+        return usersQuote;
     }
 
 
